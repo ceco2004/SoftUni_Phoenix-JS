dd808113cfba7b2c64349d9f8d705f8cec7116e0
ceco2004 <ceco2004@abv.bg>
2022-11-11 21:52:43 +0200

    Add project

---
+++ 02. Arrays/01. SumFirsLast.js
@@ -0,0 +1,6 @@
+function sumFirsAndLast(arr){
+    return Number(arr[0]) + Number(arr[arr.length - 1])
+}
+
+console.log(sumFirsAndLast(['20', '30', '40']));
+console.log(sumFirsAndLast(['5', '10']));
