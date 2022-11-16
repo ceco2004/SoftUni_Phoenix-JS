033c56f7a59e595895da6bf0e6696e4fbd91e7f5
ceco2004 <ceco2004@abv.bg>
2022-11-16 11:10:54 +0200

    Add solution with forEach

--- 02. Arrays/02. colectEvenPosition.js
+++ 02. Arrays/02. colectEvenPosition.js
@@ -8,5 +8,18 @@ let collect = (arr)=>{
     return tempArr;
 }
 
+let collect2 = (arr) => {
+    let tempArr = [];
+    arr.forEach((e, i, a) => {
+        if(i % 2 === 0){
+            tempArr.push(e);
+        }
+    });
+    return tempArr;
+}
+
 console.log(collect(['20', '30', '40']).join(" "));
 console.log(collect(['5', '10']).join(" "));
+
+console.log(collect2(['20', '30', '40']).join(" "));
+console.log(collect2(['5', '10']).join(" "));
