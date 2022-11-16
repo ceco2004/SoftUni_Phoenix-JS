2785c2a7cea278da57e31db2fc5548d9441ba398
ceco2004 <ceco2004@abv.bg>
2022-11-16 11:32:13 +0200

    Add solution with reduce

--- 02. Arrays/07. biggestElementInMatrix.js
+++ 02. Arrays/07. biggestElementInMatrix.js
@@ -10,11 +10,35 @@ let biggestElement = (matrix) => {
     return biggest;
 }
 
+
+let biggestElement2 = (matrix) => {
+    let temp = [];
+  
+    let biggest = (arr) => {
+        return arr.reduce((a, e) => a > e ? a : e);
+    }
+    
+    matrix.forEach(row => {
+        temp.push(biggest(row));
+        })
+
+    return biggest(temp);
+}
+
 console.log(biggestElement([[20, 50, 10],
     [8, 33, 145]]
    ));
 
 console.log(biggestElement([[3, 5, 7, 12],
+    [-1, 4, 33, 2],
+    [8, 3, 0, 4]]
+   ));
+
+   console.log(biggestElement2([[20, 50, 10],
+    [8, 33, 145]]
+   ));
+
+console.log(biggestElement2([[3, 5, 7, 12],
     [-1, 4, 33, 2],
     [8, 3, 0, 4]]
    ));
