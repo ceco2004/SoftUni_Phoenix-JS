0cde46442079954b08420497b8f2fd1a50cb7d7c
ceco2004 <ceco2004@abv.bg>
2022-11-16 11:20:55 +0200

    Add project

---
+++ 02. Arrays/07. biggestElementInMatrix.js
@@ -0,0 +1,20 @@
+let biggestElement = (matrix) => {
+    let biggest = Number.MIN_VALUE;
+    matrix.forEach(row => {
+        row.forEach(e => {
+            if(e > biggest){
+                biggest = e;
+            }
+        })
+    });
+    return biggest;
+}
+
+console.log(biggestElement([[20, 50, 10],
+    [8, 33, 145]]
+   ));
+
+console.log(biggestElement([[3, 5, 7, 12],
+    [-1, 4, 33, 2],
+    [8, 3, 0, 4]]
+   ));
