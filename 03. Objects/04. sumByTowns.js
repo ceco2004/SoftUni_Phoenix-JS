c4407f8ae10416f56771d196f6a958ce2d12728b
ceco2004 <ceco2004@abv.bg>
2022-11-17 19:31:14 +0200

    Add project

---
+++ 03. Objects/04. sumByTowns.js
@@ -0,0 +1,32 @@
+sumByTowns(['Sofia',
+'20',
+'Varna',
+'3',
+'Sofia',
+'5',
+'Varna',
+'4']
+);
+
+function sumByTowns(arr){
+    const objects = [];
+    arr.forEach((e, index, a) => {
+        if(index % 2 === 0){
+          
+        let town = a[index];
+        let population = +a[index + 1];
+     
+        if(objects.some(e => e.town === town)){
+            let index = objects.findIndex(o => o.town === town);
+            let obj = objects[index];
+            obj.population += population;
+        }
+        else
+        {
+            objects.push({town: town, population: population});
+        }
+    }
+    })
+
+    console.log(JSON.stringify(objects))
+}
