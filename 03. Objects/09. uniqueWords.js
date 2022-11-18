85e206f55ff4494107cff7f06639f582d39e53ca
ceco2004 <ceco2004@abv.bg>
2022-11-18 20:06:43 +0200

    Add project

---
+++ 03. Objects/09. uniqueWords.js
@@ -0,0 +1,31 @@
+let arr = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Pellentesque quis hendrerit dui.', 
+'Quisque fringilla est urna, vitae efficitur urna vestibulum fringilla.', 
+'Vestibulum dolor diam, dignissim quis varius non, fermentum non felis.', 
+'Vestibulum ultrices ex massa, sit amet faucibus nunc aliquam ut.', 
+'Morbi in ipsum varius, pharetra diam vel, mattis arcu.', 
+'Integer ac turpis commodo, varius nulla sed, elementum lectus.', 
+'Vivamus turpis dui, malesuada ac turpis dapibus, congue egestas metus.'];
+
+extractUnique(arr);
+
+function extractUnique(arr){
+    const uniqueWords = [];
+    arr.forEach(line => {
+        let words = line.split(/[ .,!?]+/gm).map(w => w.toLowerCase())
+                    .filter(w => w);
+
+        words.forEach(w => {
+            if(uniqueWords.some(wo => wo === w)){
+
+            }
+            else{
+                uniqueWords.push(w);
+            }
+        })            
+       
+        
+    })
+
+    console.log(uniqueWords.join(", "))
+
+}
