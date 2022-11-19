6028761b943ecf0e6408df9f38d24eb30bc49304
ceco2004 <ceco2004@abv.bg>
2022-11-19 12:21:44 +0200

    Add escape function

--- 03. Objects/Ex02. JSONtable.js
+++ 03. Objects/Ex02. JSONtable.js
@@ -15,12 +15,22 @@ function converToTable(arr){
     tableArr.push("<table>");
     objects.forEach(obj => {
         tableArr.push("	<tr>");
-        tableArr.push(`		<td>${obj.name}</td>`);
-        tableArr.push(`		<td>${obj.position}</td>`);
-        tableArr.push(`		<td>${obj.salary}</td>`);
+        tableArr.push(`		<td>${escapeHtml(obj.name)}</td>`);
+        tableArr.push(`		<td>${escapeHtml(obj.position)}</td>`);
+        tableArr.push(`		<td>${escapeHtml(obj.salary)}</td>`);
         tableArr.push(" </tr>");
     })
     tableArr.push("</table>");
 
     console.log(tableArr.join("\n"));
 }
+
+function escapeHtml(unsafe)
+{
+    return unsafe.toString()
+         .replaceAll(/&/g, "&amp;")
+         .replaceAll(/</g, "&lt;")
+         .replaceAll(/>/g, "&gt;")
+         .replaceAll(/"/g, "&quot;")
+         .replaceAll(/'/g, "&#039;");
+ }
