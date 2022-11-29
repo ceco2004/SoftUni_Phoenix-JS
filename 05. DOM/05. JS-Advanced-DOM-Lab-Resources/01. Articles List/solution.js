1ce22afea3b845cd0211be9baa3382c477caef08
ceco2004 <ceco2004@abv.bg>
2022-11-29 20:51:12 +0200

    Add empty value chek

--- 05. DOM/05. JS-Advanced-DOM-Lab-Resources/01. Articles List/solution.js
+++ 05. DOM/05. JS-Advanced-DOM-Lab-Resources/01. Articles List/solution.js
@@ -4,17 +4,19 @@ function createArticle() {
 	let title = titleEl.value;
 	let content = contentEl.value;
 
-	let articles = document.getElementById("articles");
+	if(title && content){
+		let articles = document.getElementById("articles");
 
-	let article = document.createElement("article");
-	articles.appendChild(article);
-
-	let h3Title = document.createElement("h3");
-	article.appendChild(h3Title);
-	h3Title.textContent = title;
-	let par = document.createElement("p");
-	par.textContent = content;
-	article.appendChild(par);
+		let article = document.createElement("article");
+		articles.appendChild(article);
+	
+		let h3Title = document.createElement("h3");
+		article.appendChild(h3Title);
+		h3Title.textContent = title;
+		let par = document.createElement("p");
+		par.textContent = content;
+		article.appendChild(par);
+	}
 	
 	titleEl.value = "";
 	contentEl.value = ""
