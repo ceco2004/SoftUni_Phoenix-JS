5515454327c636762d456ccd78fd670b3000f516
ceco2004 <ceco2004@abv.bg>
2022-11-29 22:40:57 +0200

    Add solution

--- 05. DOM/05. JS-Advanced-DOM-Lab-Resources/02. Format the Text/solution.js
+++ 05. DOM/05. JS-Advanced-DOM-Lab-Resources/02. Format the Text/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO...
+  
+  let inputEl = document.getElementById("input");
+  let stringArr = inputEl.textContent.split(".").map((e, i, a) => i !== (a.length - 1) ? e + "." : e);
+  inputEl.textContent = "";
+  let outputDiv = document.getElementById("output");
+
+  let len = stringArr.length;
+  for(let i = 0; i < len; i += 3){
+    let par = document.createElement("p");
+    let contentP = stringArr[i];
+     if(i + 1 < len){
+      contentP += stringArr[i + 1];
+     }
+
+     if(i + 2 < len){
+      contentP += stringArr[i + 2];
+     }
+
+     par.textContent = contentP;
+     outputDiv.appendChild(par);
+
+  }
+
 }
