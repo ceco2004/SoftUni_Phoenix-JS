1d63f7ada3f9c3c1ccad8abd384a205f0dc82d1f
ceco2004 <ceco2004@abv.bg>
2022-12-01 19:51:24 +0200

    Add solution

--- 05. DOM/05. JS-Advanced-DOM-Lab-Resources/04. Visited Websites/solution.js
+++ 05. DOM/05. JS-Advanced-DOM-Lab-Resources/04. Visited Websites/solution.js
@@ -1,4 +1,16 @@
 function solve() {
 
-  //TODO...
+  let divMiddlet = document.querySelector(".middled");
+  divMiddlet.addEventListener("click", clickedLink);
+
+}
+
+function clickedLink(e){
+  let node = e.target.parentNode.nextElementSibling;
+  let par = node.textContent;
+
+  let dataParent = par.split(" ");
+  +dataParent[1] ++;
+  par = dataParent.join(" ");
+  node.textContent = par;
 }
