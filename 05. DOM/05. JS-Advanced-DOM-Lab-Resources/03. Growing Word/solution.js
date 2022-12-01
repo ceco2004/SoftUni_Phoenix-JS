65b30ce6bb3dec564e580c0114c37ea43fbdc1f9
ceco2004 <ceco2004@abv.bg>
2022-12-01 14:39:44 +0200

    Add solution

--- 05. DOM/05. JS-Advanced-DOM-Lab-Resources/03. Growing Word/solution.js
+++ 05. DOM/05. JS-Advanced-DOM-Lab-Resources/03. Growing Word/solution.js
@@ -1,4 +1,38 @@
 function growingWord() {
 
-  //TODO...
+  let paragrapfs = document.querySelectorAll(".conditions p");
+  paragrapfs.forEach(p => {
+    let style = p.getAttribute("style");
+    if(style){
+      let data = style.split(" ");
+      console.log(data);
+      let size = +data[1].replace("px;", "").trim();
+      let color = data[3].trim();
+      
+  
+      switch(color){
+        case "blue": color = "green";
+          break;
+        case "green": color = "red";
+          break;
+        case "red": color = "blue";
+          break;
+      }
+  
+      if(size){
+        size += +size;
+      }
+      else{
+        size = 2;
+      }
+  
+      p.setAttribute("style", `font-size: ${size}px; color: ${color}`);
+    }
+    else{
+      p.setAttribute("style", "font-size: 2px; color: green")
+    }
+
+  })
+  
+ 
 }
