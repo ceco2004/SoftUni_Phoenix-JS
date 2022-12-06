6f193eb7626457f67b9fe9b83da630516512d3cb
ceco2004 <ceco2004@abv.bg>
2022-12-06 20:15:01 +0200

    Add solution

--- 05. DOM/05. JS-Advanced-DOM-Exercise-Resources/01. Chat-Room/solution.js
+++ 05. DOM/05. JS-Advanced-DOM-Exercise-Resources/01. Chat-Room/solution.js
@@ -1,5 +1,17 @@
 function solve() {
-   //TODO...
+  let sendBtn = document.getElementById("send");
+  sendBtn.addEventListener("click", sendMsg);
 }
 
+function sendMsg(e){
+   let textArea = document.getElementById("chat_input");
+   let msg = textArea.value;
+   textArea.value = "";
+
+   let divContainer = document.getElementById("chat_messages");
+   let newDiv = document.createElement("div");
+   newDiv.classList.add("message", "my-message");
+   newDiv.textContent = msg;
+   divContainer.appendChild(newDiv);
+}
 
