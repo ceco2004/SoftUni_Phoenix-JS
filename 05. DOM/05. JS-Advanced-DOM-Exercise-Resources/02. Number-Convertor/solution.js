d10bde85d50aaef04f613998707484d7765b72ba
ceco2004 <ceco2004@abv.bg>
2022-12-06 20:33:56 +0200

    Add solution

--- 05. DOM/05. JS-Advanced-DOM-Exercise-Resources/02. Number-Convertor/solution.js
+++ 05. DOM/05. JS-Advanced-DOM-Exercise-Resources/02. Number-Convertor/solution.js
@@ -1,4 +1,32 @@
 function solve() {
+    let btn = document.getElementsByTagName("button")[0];
+    btn.addEventListener("click", convert);
+}
 
-    //TODO...
+function convert(e){
+    let inputEl = document.getElementById("input");
+    let inputValue = +inputEl.value;
+  
+
+    let outputCurency = document.getElementById("selectMenuTo").value;
+    let outputValue;
+
+    switch(outputCurency){
+        case "binary": outputValue = convertToBinary(inputValue);
+           break;
+        case "hexadeicmal": outputValue = convertToHexa(inputValue);
+           break;
+    }
+
+    let resultArea = document.getElementById("result");
+    resultArea.value = outputValue;
+
+}
+
+function convertToBinary(decimal){
+    return decimal.toString(2);
+}
+
+function convertToHexa(decimal){
+    return decimal.toString(16);
 }
