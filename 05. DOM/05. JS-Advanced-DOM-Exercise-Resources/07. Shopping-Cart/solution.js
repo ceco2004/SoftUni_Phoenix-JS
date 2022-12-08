451995ec8ff6d5217366893eb55fc2b14b56347a
ceco2004 <ceco2004@abv.bg>
2022-12-08 22:42:53 +0200

    Add solution to shopping cart

--- 05. DOM/05. JS-Advanced-DOM-Exercise-Resources/07. Shopping-Cart/solution.js
+++ 05. DOM/05. JS-Advanced-DOM-Exercise-Resources/07. Shopping-Cart/solution.js
@@ -1,3 +1,45 @@
 function solve() {
-   //TODO...
+   let shoppingCartDiv = document.querySelector("body div");
+   shoppingCartDiv.addEventListener("click", addProduct);
+
+   let checkoutbtn = document.querySelector(".checkout");
+   checkoutbtn.addEventListener("click", checkout);
+
+   let text = "";
+   let textArea = document.getElementsByTagName("textarea")[0];
+
+   let totalPrice = 0;
+   let productsArr = [];
+
+
+
+   function addProduct(e){
+      let element = e.target;
+   
+      if(element.classList.contains("add-product")){
+          let parentDiv = element.parentNode;
+          let product = parentDiv.previousElementSibling.firstElementChild.textContent;
+          let price = +parentDiv.nextElementSibling.textContent;
+
+          totalPrice += price;
+          if(productsArr.some(p => p === product) == false){
+            productsArr.push(product);
+          }
+          
+          text += `Added ${product} for ${price} to the cart.\n`;
+          textArea.textContent = text;
+   
+          
+      }
+   }
+
+
+   function checkout(e){
+      let btns = Array.from(document.getElementsByTagName("button"));
+      btns.forEach(b => (b.setAttribute("disabled", "true"), b.classList.add("disable-btn")));
+      let msg = `You bought ${productsArr.join(", ")} for ${totalPrice.toFixed(2)}.`;
+      textArea.textContent += msg;
+   }
+
 }
+
