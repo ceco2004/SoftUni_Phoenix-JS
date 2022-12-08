a49fbf8cb698034e92686184824d5ef8d7e0fcdc
ceco2004 <ceco2004@abv.bg>
2022-12-08 21:00:32 +0200

    Add solution to searching in page

--- 05. DOM/05. JS-Advanced-DOM-Exercise-Resources/06. Table-Search-Engine/solution.js
+++ 05. DOM/05. JS-Advanced-DOM-Exercise-Resources/06. Table-Search-Engine/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-   //TODO...
+   let searchBtn = document.getElementById("searchBtn");
+   searchBtn.addEventListener("click", search);
+}
+
+function search(){
+
+   let searchArea = document.getElementsByTagName("input")[0];
+   let searchCriteria = searchArea.value.trim();
+   searchArea.value = "";
+
+   let tableTr = Array.from(document.getElementsByTagName("tbody")[0].children);
+   clearPreviosSeach();
+
+   if(searchCriteria){
+      tableTr.forEach(tr => {
+         let tdS = Array.from(tr.children);
+         tdS.forEach(e => {
+            let content = e.textContent;
+            console.log(content)
+            if(content.toLocaleLowerCase().indexOf(searchCriteria.toLowerCase()) >= 0){
+               tr.classList.add("select");
+            }
+         })
+   
+      })
+   }
+
+   function clearPreviosSeach(){
+      tableTr.forEach(tr => {
+               tr.classList.remove("select");
+            })
+   }
 }
