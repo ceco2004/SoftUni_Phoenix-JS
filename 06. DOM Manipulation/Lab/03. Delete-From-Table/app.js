d92e9c760cce57c5b79843b9207b41286616da32
ceco2004 <ceco2004@abv.bg>
2022-12-10 21:11:07 +0200

    Add solution to remove from teble

--- 06. DOM Manipulation/Lab/03. Delete-From-Table/app.js
+++ 06. DOM Manipulation/Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,23 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+   let searchElement = document.querySelector("input");
+   let criteria = searchElement.value;
+   searchElement.value = "";
+
+   let table = document.querySelector("tbody");
+   let rows = Array.from(table.children);
+
+   let haveSuccess = false;
+   rows.forEach(r => {
+    Array.from(r.children).forEach(ch => {
+        if(ch.textContent === criteria){
+            r.remove();
+            haveSuccess = true;
+        }
+    })
+   })
+
+   if(haveSuccess === false){
+    document.getElementById("result").textContent = "Not found.";
+   }
+   
 }
