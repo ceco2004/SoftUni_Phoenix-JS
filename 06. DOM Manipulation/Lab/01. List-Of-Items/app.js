19b09a4e280558ad489b366d107047195dc48b51
ceco2004 <ceco2004@abv.bg>
2022-12-10 20:58:02 +0200

    Add solution to add and delete list items

--- 06. DOM Manipulation/Lab/01. List-Of-Items/app.js
+++ 06. DOM Manipulation/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,32 @@
+(function listenerUl(){
+    document.getElementById("items").addEventListener("click", removeElement);
+})();
+
+function removeElement(e){
+    
+    let zone = e.target;
+    if(zone.textContent === "[Delete]"){
+        let parent = zone.parentNode;
+        parent.remove();
+    }
+}
+
+
 function addItem() {
-    console.log('TODO:...');
+    let input = document.getElementById("newItemText");
+    let text = input.value;
+    text += `<a href="#">[Delete]</a>`;
+
+    input.value = "";
+
+    let newLi = document.createElement("li");
+    newLi.innerHTML = text;
+
+    let ul = document.getElementById("items");
+    ul.appendChild(newLi);
+}
+
+function remove(e){
+    e.preventDefault;
+   e.target.remove;
 }
