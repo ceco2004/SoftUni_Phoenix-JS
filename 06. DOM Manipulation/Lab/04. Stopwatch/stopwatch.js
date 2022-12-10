56a30dc02dcbfb7727e47a25b4b19130c6fcb2cd
ceco2004 <ceco2004@abv.bg>
2022-12-10 21:58:24 +0200

    Add solution to stopwatch

--- 06. DOM Manipulation/Lab/04. Stopwatch/stopwatch.js
+++ 06. DOM Manipulation/Lab/04. Stopwatch/stopwatch.js
@@ -1,3 +1,51 @@
 function stopwatch() {
-    console.log('TODO:...');
+    let startBtn = document.getElementById("startBtn");
+    startBtn.addEventListener("click", start);
+
+    let stopBtn = document.getElementById("stopBtn");
+    stopBtn.addEventListener("click", stop);
+
+    let taimer;
+    let seconds = 0;
+
+    function start(){
+        startBtn.setAttribute("disabled", "true");
+        stopBtn.removeAttribute("disabled");
+        taimer = setInterval(increseWithSecond, 1000);
+    }
+
+    function increseWithSecond(){
+        seconds++;
+        let time = convertToTime();
+        timeToDOM(time);
+    }
+
+    function convertToTime(){
+        let min = Math.floor(seconds / 60);
+        let sec = seconds % 60 ;
+
+        if(min < 10){
+            min = "0"+ min;
+        }
+
+        if(sec < 10){
+            sec = "0" + sec;
+        }
+
+       return `${min}:${sec}`;
+    }
+
+    function timeToDOM(time){
+        console.log(time)
+        document.getElementById("time").textContent = time;
+
+    }
+
+    function stop(){
+        clearInterval(taimer);
+        seconds = 0;
+        timeToDOM("00:00");
+        stopBtn.setAttribute("disabled", "true");
+        startBtn.removeAttribute("disabled");
+    }
 }
