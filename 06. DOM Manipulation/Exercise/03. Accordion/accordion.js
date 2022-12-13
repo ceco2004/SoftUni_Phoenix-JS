996df98b01adc29ee091eb509b0cd8c153438121
ceco2004 <ceco2004@abv.bg>
2022-12-13 19:51:19 +0200

    Add solution to According

--- 06. DOM Manipulation/Exercise/03. Accordion/accordion.js
+++ 06. DOM Manipulation/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,24 @@
 function toggle() {
-    console.log('TODO:...');
+    let btn = document.querySelector("span");
+    let btnText = btn.textContent;
+
+    let divContainer = document.getElementById("extra");
+
+    switch(btnText){
+        case "Less": showLess();
+            break;
+        case "More": showMore();
+            break;
+    }
+
+    function showLess(){
+        btn.textContent = "More";
+        divContainer.setAttribute("style", "display: none");
+    }
+
+    function showMore(){
+        btn.textContent = "Less";
+        divContainer.setAttribute("style", "display: block")
+    }
+
 }
