5a3c0cb9fdcbaf93c62d30323ff84b8f4d525b4e
ceco2004 <ceco2004@abv.bg>
2022-12-13 22:50:30 +0200

    Add solution to Locked profile

--- 06. DOM Manipulation/Exercise/07. Locked-Profile/app.js
+++ 06. DOM Manipulation/Exercise/07. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+   let btns = Array.from(document.querySelectorAll("button"));
+   btns.forEach(b => b.addEventListener("click", showMore));
+
+   function showMore(){
+        let hiden = this.previousElementSibling;
+        
+        if(this.textContent === "Hide it"){
+            this.textContent = "Show more";
+            hiden.removeAttribute("style");
+        }
+        else if(this.textContent === "Show more"){
+            
+            let lockRadio = this.parentNode.children[2]
+            if(lockRadio.checked === false){
+                this.textContent = "Hide it";
+                hiden.setAttribute("style", "display: block");
+            }
+        }
+       
+   }
 }
