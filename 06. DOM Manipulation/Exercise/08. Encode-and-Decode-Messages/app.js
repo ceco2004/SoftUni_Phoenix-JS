35a2877a0388e2ab029caf2d2774c01df98fad2b
ceco2004 <ceco2004@abv.bg>
2022-12-13 23:25:35 +0200

    Add solution

--- 06. DOM Manipulation/Exercise/08. Encode-and-Decode-Messages/app.js
+++ 06. DOM Manipulation/Exercise/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,34 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let btns = document.querySelectorAll("button");
+    btns[0].addEventListener("click", encodeMsg);
+    btns[1].addEventListener("click", decodeMsg);
+
+    let areas = Array.from(document.querySelectorAll("textarea"));
+    let forEncoding = areas[0];
+    let forDecoding = areas[1];
+    
+    function encodeMsg(){
+       let text = forEncoding.value;
+       forEncoding.value = "";
+
+       let result = [];
+       Array.from(text).forEach(e => {
+           result.push(String.fromCharCode(e.charCodeAt() + 1))
+       })
+       forDecoding.value = result.join("");
+    }
+
+    function decodeMsg(){
+        let text = forDecoding.value;
+        forDecoding.value = "";
+
+        let result = [];
+        Array.from(text).forEach(e => {
+            result.push(String.fromCharCode(e.charCodeAt() - 1));
+        })
+
+        forEncoding.value = result.join("");
+    }
+
 }
+
