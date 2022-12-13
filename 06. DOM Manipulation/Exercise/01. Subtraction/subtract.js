41939d58078fdeb313d98e465d459d9301658246
ceco2004 <ceco2004@abv.bg>
2022-12-13 19:08:59 +0200

    Add solution to Substraction exersise

--- 06. DOM Manipulation/Exercise/01. Subtraction/subtract.js
+++ 06. DOM Manipulation/Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,5 @@
 function subtract() {
-    console.log('TODO:...');
+    let num1 = +document.getElementById("firstNumber").value;
+    let num2 = +document.getElementById("secondNumber").value;
+    document.getElementById("result").textContent = num1 - num2;
 }
