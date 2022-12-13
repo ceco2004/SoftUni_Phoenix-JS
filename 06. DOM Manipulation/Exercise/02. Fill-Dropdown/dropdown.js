c5c919b8b9c6360738a2ac3110286b83ed4d893a
ceco2004 <ceco2004@abv.bg>
2022-12-13 19:23:16 +0200

    Add solution

--- 06. DOM Manipulation/Exercise/02. Fill-Dropdown/dropdown.js
+++ 06. DOM Manipulation/Exercise/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,17 @@
 function addItem() {
-    console.log('TODO:...');
+   let textEl = document.getElementById("newItemText");
+   let valueEl = document.getElementById("newItemValue");
+
+   let text = textEl.value;
+   let value = valueEl.value;
+   textEl.value = valueEl.value = "";
+
+   let optionEl = document.createElement("option");
+   let content = `${text} - ${value}`;
+   optionEl.textContent = content;
+   optionEl.setAttribute("value", content);
+
+   let selectEl = document.getElementById("menu");
+   selectEl.appendChild(optionEl);
+
 }
