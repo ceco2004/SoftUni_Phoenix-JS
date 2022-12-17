d98c582f62670e75b2615266c06c135bddd1ea9a
ceco2004 <ceco2004@abv.bg>
2022-12-17 20:06:48 +0200

    Edit toString() to replace undefind with corect value

--- 07. Classes/02. person.js
+++ 07. Classes/02. person.js
@@ -7,9 +7,24 @@ class Person {
     }
 
     toString() {
-        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        let fName = Boolean(this.firstName) ? this.firstName : "";
+        let lName = Boolean(this.lastName) ? this.lastName : "";
+        let age = Boolean(this.age) ? this.age : "-not present-";
+        let email = Boolean(this.email) ? this.email :  "-not-present-";
+        return `${fName} ${lName} (age: ${age}, email: ${email})`;
     }
 }
+
+function get4People(){
+    return [
+        new Person("Anna", "Simpson", 22, "anna@yahoo.com"),
+        new Person("SoftUni"),
+        new Person("Stephan", "Johnson", 25),
+        new Person("Gabriel", "Peterson", 24, "g.p@gmail.com")
+    ]
+}
+
+get4People().forEach(p => console.log(p.toString()));
 
 let p1 = new Person("Ivan", "Petrov", 23, "some@mail.bg");
 let p2 = new Person("Monika", "Vasileva", 25, "monika@abv.bg");
