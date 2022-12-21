8c25eca1be0806e62b9ec975fd577a02082912a1
ceco2004 <ceco2004@abv.bg>
2022-12-21 21:28:46 +0200

    Edit mixin function to apply to prototipe of the Class

--- 07. Classes/demo_miksin.js
+++ 07. Classes/demo_miksin.js
@@ -12,14 +12,26 @@ class Teacher extends Person {
     }
 }
 
-function extendPerson(){
-    this.getInfo = function (){
-        return `${this.name} is ${this.age} years old.`;
+function extendPerson (){
+   this.getInfo = function (){
+    let info = "";
+    for(let key in this){
+        if(typeof(this[key]) === "function"){
+            continue;
+        }
+        info += `${key} - ${this[key]}; `;
+    }
+        return info;
     }
     return this;
 }
 
 let p = new Person("Ivan", 23);
+let p2 = new Person("Pesho", 22);
+let t = new Teacher("Gosho", 41, "Math");
 
-extendPerson.call(p);
+extendPerson.call(Person.prototype);
+
 console.log(p.getInfo());
+console.log(p2.getInfo());
+console.log(t.getInfo());
