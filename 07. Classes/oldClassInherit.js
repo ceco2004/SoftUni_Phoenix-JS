8bb141ac5ab35871c61bba051379c400e683f36e
ceco2004 <ceco2004@abv.bg>
2022-12-22 21:28:00 +0200

    Add method via parent, and child class name

--- 07. Classes/oldClassInherit.js
+++ 07. Classes/oldClassInherit.js
@@ -4,14 +4,18 @@ function Person(fn, ln, age){
     this.age = age;
 
        this. personInfo = function() { 
-        return `My name is ${this.firstName} ${this.lastName}, and I'm ${this.age} Years old.`
+        return `My name is ${this.firstName} ${this.lastName}, and I'm ${this.age} Years old.`;
     }
-
 }
 
 function Teacher(fn, ln, age, special){
     Person.call(this, fn, ln, age);
     this.special = special;
+    this.constructor = Teacher;
+    
+    this. personInfo = function() { 
+        return `My name is ${this.firstName} ${this.lastName}, and I'm ${this.age} Years old, and my speciality is ${this.special}.`
+    }
 }
 
 Teacher.prototype = Object.create(Person.prototype);
@@ -19,5 +23,22 @@ Teacher.prototype = Object.create(Person.prototype);
 let p = new Person("Ceco", "Ivanov", 23);
 let t = new Teacher("Pesho", "Goshov", 43, "Phisics");
 
+Person.prototype.obInfo = function(){
+    return `${this.constructor.name}`
+}
+console.log("personInfo()");
+
 console.log(p.personInfo());
 console.log(t.personInfo());
+
+console.log("==============================================");
+console.log("log object");
+
+console.log(p);
+console.log(t);
+
+console.log("==============================================");
+console.log("log objInfo()");
+
+console.log(p.obInfo());
+console.log(t.obInfo());
